6f9b0b0201951ddf4adb4a31d9672f605a45d1f6
MineDen <dengr1065@gmail.com>
2019-03-01 18:16:49 +0200

    added simple readme

---
+++ README.md
@@ -0,0 +1,4 @@
+![winfuck logo](https://raw.githubusercontent.com/dengr1065/winfuck/master/readme/logo.png "logo")
+
+---
+Try compiling it :P
--- source.cpp
+++ source.cpp
@@ -3,10 +3,10 @@
 int main(int argc, char *argv[])
 {
     DWORD threadIdFix;
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)randCursor, NULL, 0, &threadIdFix);
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)invertScreen, NULL, 0, &threadIdFix);
+    //CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)randCursor, NULL, 0, &threadIdFix);
+    //CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)invertScreen, NULL, 0, &threadIdFix);
     CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)swapMouseBtns, NULL, 0, &threadIdFix);
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)drawErrorIcons, NULL, 0, &threadIdFix);
+    //CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)drawErrorIcons, NULL, 0, &threadIdFix);
 
     moveFgWin();
 }
